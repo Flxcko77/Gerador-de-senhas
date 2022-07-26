d9d6dff67713ae507e565adb3179f602966874a4
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2022-07-26 18:13:53 -0300

    :wrench: adicionando funções

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function Gerar(){
+    let GerarNovamente = window.document.getElementById('Gerar');
+    let Resultado = window.document.getElementById('Resultado');
+    let password = Math.random().toString(36).substr(2);
+    password = password.substr(0, 10);
+    GerarNovamente.value = `GERAR NOVAMENTE`;
+    Resultado.innerHTML = `${password}`;
+
+}
+function Copiar(){
+    let Copiado = window.document.getElementById('Copiar');
+    let SenhaGerada = window.document.getElementById('Resultado');
+    let SenhaCopiada = SenhaGerada.innerText;
+    Copiado.value = `COPIADO!`;
+    
+}
+
+function Sair(){
+    let Copiar = window.document.getElementById('Copiar');
+    Copiar.value = `COPIAR`;
+}
