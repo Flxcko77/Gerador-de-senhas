fb3609f4d0262828c4d36c0eaa09d77702feb9a2
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2022-08-02 12:21:09 -0300

    :bug: consertando bug

--- script.js
+++ script.js
@@ -5,19 +5,16 @@ function Gerar(){
     let password = Math.random().toString(36).substr(2);
     password = password.substr(0, 10);
     GerarNovamente.value = `GERAR NOVAMENTE`;
-    Resultado.innerHTML = `${password}`;
+    Resultado.value = `${password.toLocaleUpperCase()}`;
 
 }
 function Copiar(){
     let Copiado = window.document.getElementById('Copiar');
     let SenhaGerada = window.document.getElementById('Resultado');
-    let SenhaCopiada = SenhaGerada.innerText;
     Copiado.value = `COPIADO!`;
-    alert(`Senha copiada ${SenhaCopiada}`);
-    
-    SenhaCopiada.select();
+    SenhaGerada.select();
     document.execCommand('copy');
-
+    
 }
 
 function Sair(){
